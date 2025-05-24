f0969e0a0988dfe20bd16da973496a6c20d6d7b1
lebel <lebel>
2025-05-24 14:24:23 +0200

    feat: create sample fact card component

---
+++ src/components/FactCard.vue
@@ -0,0 +1,20 @@
+<script setup lang="ts">
+  interface FactCardProps {
+    title: string,
+    description: string
+  }
+  defineProps<FactCardProps>();
+</script>
+
+<template>
+<div class="card">
+  <div class="card-body">
+    <p>A cool {{title}} Fact</p>
+    <p>{{description}}</p>
+  </div>
+</div>
+</template>
+
+<style scoped>
+
+</style>
